9d0f5f5abdab8b1f3bb792f6d1efb533c66501a1
ervin pratama <vinpratama12@gmail.com>
2022-09-28 15:53:05 +0700

    New Landing Page

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -1,52 +1,254 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <meta name="description" content=""/>
-    <meta name="author" content=""/>
-    <title>KOMINFO</title>
-    <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico"/>
-    <!-- Font Awesome icons (free version)-->
-    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-    <!-- Google fonts-->
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css"/>
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css"/>
-    <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="{{asset('landing/css/styles.css')}}" rel="stylesheet"/>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Dinas Komunikasi Dan Informatika Situbondo</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Facebook Opengraph integration: https://developers.facebook.com/docs/sharing/opengraph -->
+    <meta property="og:title" content="">
+    <meta property="og:image" content="">
+    <meta property="og:url" content="">
+    <meta property="og:site_name" content="">
+    <meta property="og:description" content="">
+
+    <!-- Twitter Cards integration: https://dev.twitter.com/cards/  -->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="">
+    <meta name="twitter:title" content="">
+    <meta name="twitter:description" content="">
+    <meta name="twitter:image" content="">
+
+    <!-- Favicons -->
+    <link href="{{asset('landing/assets/img/favicon.png')}}" rel="icon">
+    <link href="{{asset('landing/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,500,700|Roboto:400,900" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="{{asset('landing/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('landing/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+    <link href="{{asset('landing/assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="{{asset('landing/assets/css/style.css')}}" rel="stylesheet">
+
+    <!-- =======================================================
+    * Template Name: Bell - v4.8.0
+    * Template URL: https://bootstrapmade.com/bell-free-bootstrap-4-template/
+    * Author: BootstrapMade.com
+    * License: https://bootstrapmade.com/license/
+    ======================================================== -->
 </head>
-<body id="page-top">
-<!-- Masthead-->
-<header class="masthead">
-    <div class="container">
-        <a class="btn btn-primary btn-xl text-uppercase" href="login">Login</a>
+
+<body>
+
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="hero">
+    <div class="container text-center">
+        <div class="col-md-12">
+            <h1>
+                Sistem Absensi Manajemen Rapat
+            </h1>
+
+            <p class="tagline">
+
+            </p>
+            <a class="btn btn-full scrollto" href="login">Mulai</a>
+        </div>
     </div>
-</header>
-<!-- Footer-->
-<footer class="footer py-4">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-lg-4 text-lg-start">Copyright &copy; Kominfo 2022</div>
-            <div class="col-lg-4 my-3 my-lg-0">
-                <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Twitter"><i
-                        class="fab fa-twitter"></i></a>
-                <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Facebook"><i
-                        class="fab fa-facebook-f"></i></a>
-                <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Instagram"><i
-                        class="fab fa-instagram"></i></a>
+
+</section><!-- End Hero -->
+
+<!-- ======= Header ======= -->
+<header id="header" class="d-flex align-items-center">
+    <div class="container d-flex align-items-center">
+
+        <div id="logo" class="me-auto">
+            <a href="index.html"><img src="assets/img/logo-nav.png" alt=""></a>
+            <!-- Uncomment below if you prefer to use a text image -->
+            <!--<h1><a href="#hero">Bell</a></h1>-->
+        </div>
+
+      
+
+        <div class="header-social-links d-flex align-items-center">
+            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+            <a href="https://www.instagram.com/kominfositubondo/" class="instagram"><i class="bi bi-instagram"></i></a>
+            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+        </div>
+    </div>
+</header><!-- End Header -->
+
+<main id="main">
+    <!-- ======= Welcome Section ======= -->
+    <section class="welcome text-center">
+        <h2>Sistem Absensi Manajemen Rapat</h2>
+        <p>
+        Sistem Absensi Manajemen Rapat digunakan<br>
+        untuk memanajemen kegiatan rapat<br>
+        di Dinas Komunikasi dan Informatika Situbondo
+        </p>
+    </section><!-- End Welcome Section -->
+
+    <!-- ======= Features Section ======= -->
+    <section class="features" id="features">
+
+        <div class="container">
+            <h2 class="text-center">
+                Fitur
+            </h2>
+
+            <div class="row">
+                <div class="feature-col col-lg-4 col-xs-12">
+                    <div class="card card-block text-center">
+                        <div>
+                            <div class="feature-icon">
+                                <i class="bi bi-briefcase"></i>
+                            </div>
+                        </div>
+
+                        <div>
+                            <h3>
+                                Notulensi
+                            </h3>
+
+                            <p>
+                                Pencatatan mengenai hal yang dibicaran dan diputuskan selama rapat berlangsung.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="feature-col col-lg-4 col-xs-12">
+                    <div class="card card-block text-center">
+                        <div>
+                            <div class="feature-icon">
+                                <i class="bi bi-card-checklist"></i>
+                            </div>
+                        </div>
+
+                        <div>
+                            <h3>
+                            Daftar Hadir
+                            </h3>
+
+                            <p>
+                            Keterangan atas kehadiran peserta dalam rapat.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="feature-col col-lg-4 col-xs-12">
+                    <div class="card card-block text-center">
+                        <div>
+                            <div class="feature-icon">
+                                <i class="bi bi-calendar4-week"></i>
+                            </div>
+                        </div>
+
+                        <div>
+                            <h3>
+                                Daftar Kegiatan
+                            </h3>
+                            <p>
+                                Berisi detail daftar kegiatan rapat yang akan dilaksanakan.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-4 text-lg-end">
-                <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
-                <a class="link-dark text-decoration-none" href="#!">Terms of Use</a>
+        </div>
+
+    </section><!-- End Features Section -->
+
+    <!-- ======= Foto Rapat ======= -->
+    <section class="team" id="team">
+        <div class="container">
+            <h2 class="text-center">
+                Dokumentasi Kegiatan Rapat
+            </h2>
+
+            <div class="row">
+                <div class="col-sm-3 col-xs-6">
+                    <div class="card card-block">
+                        <img alt="" class="team-img" src="{{asset('landing/assets/img/rapat-1.webp')}}">
+                            <div class="card-title-wrap">
+                                <span class="card-title">Rapat 1</span> <span class="card-text">Rapat BBM</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-sm-3 col-xs-6">
+                    <div class="card card-block">
+                        <img alt="" class="team-img" src="{{asset('landing/assets/img/rapat-1.webp')}}">
+                            <div class="card-title-wrap">
+                                <span class="card-title">Rapat 2</span> <span class="card-text">Rapat Korupsi</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-sm-3 col-xs-6">
+                    <div class="card card-block">
+                        <img alt="" class="team-img" src="{{asset('landing/assets/img/rapat-1.webp')}}">
+                            <div class="card-title-wrap">
+                                <span class="card-title">Rapat 3</span> <span class="card-text">Rapat Kegiatan</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-sm-3 col-xs-6">
+                    <div class="card card-block">
+                        <img alt="" class="team-img" src="{{asset('landing/assets/img/rapat-1.webp')}}">
+                            <div class="card-title-wrap">
+                                <span class="card-title">Rapat 4</span> <span class="card-text">Rapat Sembako</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Team Section -->
+</main><!-- End #main -->
+
+<!-- ======= Footer ======= -->
+<footer class="site-footer">
+    <div class="bottom">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-10 col-xs-12 text-lg-start text-center">
+                    <p class="copyright-text">
+                        &copy; Copyright <strong>2020, Dinas Komunikasi dan Informatika Kab. Situbondo.</strong>. All Rights Reserved
+                    </p>
+                </div>
             </div>
         </div>
     </div>
-</footer>
-<!-- Bootstrap core JS-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-<!-- Core theme JS-->
-<script src="{{asset('landing/js/scripts.js')}}"></script>
-<script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+</footer><!-- End Footer -->
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="{{asset('landing/assets/vendor/purecounter/purecounter_vanilla.js')}}"></script>
+<script src="{{asset('landing/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('landing/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+<script src="{{asset('landing/assets/vendor/php-email-form/validate.js')}}"></script>
+
+<!-- Template Main JS File -->
+<script src="{{asset('landing/assets/js/main.js')}}"></script>
+
 </body>
+
 </html>
