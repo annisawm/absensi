4d997e7601cb47a5c1d6f51d852fc0f6c35a7519
annisawm <annisawm200601@gmail.com>
2022-11-01 15:20:24 +0700

    edit ttd for cetak blade

---
+++ resources/views/guest/cetak.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<h3><center>Laporan Daftar Hadir</center></h3>
+<style>
+    .signed{
+        width: 100%;
+    }
+</style>
+<table border="1" cellspacing="0" cellpadding="5">
+    <tr>
+        <th>NIP</th>
+        <th>Nama</th>
+        <th>Jenis Kelamin</th>
+        <th>Nama Instansi(OPD)</th>
+        <th>Jabatan</th>
+        <th>No. Hp</th>
+        <th>TTD</th>
+    </tr>
+    @foreach($guest as $g)
+        <tr>
+            <td>{{$g->nip}}</td>
+            <td>{{$g->nama}}</td>
+            <td>{{$g->jenis_kelamin}}</td>
+            <td>{{$g->opds->nama_opd}}</td>
+            <td>{{$g->jabatan}}</td>
+            <td>{{$g->no_hp}}</td>
+            <td><img class="signed" src="{{storage_path('app/' . $g->signed)}}" alt="user image"></td>
+        </tr>
+    @endforeach
+</table>
+</html>
